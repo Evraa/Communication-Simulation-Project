25915312946ef20d04a4efbcc76c057e714bb919
Mahmoud Adas <mido3ds@gmail.com>
2019-12-15 18:38:36 +0200

    in fm.py, fixed mp and max_freq

--- fm.m
+++ fm.m
@@ -1,24 +1,18 @@
-1; 
-pkg load communications;
 %
 % FM modulation
-% author: Mahmoud
+% author: Mahmoud Adas
 % 
+
+1; % script
+
+pkg load communications;
+pkg load miscellaneous;
 
 % carson rule:
 % kf = β * B * 2π / mp
-function kf = calc_kf(beta, m)
-    % TODO: compute B
-    % get B (max frequency)
+function kf = calc_kf(beta, m, sample_rate)
+    max_freq = sample_rate/2;
 
-    % spectrum = fft(m)
-    % freq = fftfreq(len(spectrum))
-    % threshold = 0.5 * max(abs(spectrum))
-    % mask = abs(spectrum) > threshold
-    % peaks = freq[mask]
-    % max_freq = peaks.max()  % B
-    max_freq = 5; % temporarily
-    
     mp = max(abs(m));
 
     kf = beta * max_freq * 2 * pi / mp;
@@ -27,7 +21,6 @@ endfunction
 % modulated(t) = Ac * cos(wc * t + kf * integration(audio(t)))
 function modulated = modulate_fm(audio, sample_rate, beta)
     % time samples
-    % TODO: cant calculate, out of memory
     t = (0:length(audio)) / sample_rate;
 
     ac = 1;
@@ -35,14 +28,16 @@ function modulated = modulate_fm(audio, sample_rate, beta)
     fc = 100 * 1000;  % 100 kHz
     wc = 2 * pi * fc;
 
-    kf = calc_kf(beta, audio);
+    kf = calc_kf(beta, audio, sample_rate);
 
-    modulated = ac * cos(wc + kf * cumsum(audio));
+    modulated = ac * cos(wc * t + kf * cumsum(audio));
 endfunction
 
 disp('read audio file');
 [audio, sample_rate] = audioread('sample.wav');
-audio = (audio(:, 1) + audio(:, 2)) / 2;
+
+disp('stereo to mono');
+audio = mean(audio(:, 1) + audio(:, 2));
 
 betas.wide = 5;
 betas.narrow = .1;
@@ -56,7 +51,7 @@ for [beta_value, name] = betas
         disp(out_file_name);
 
         % add noise
-        modulated_with_noise = awgn(modulated, snr);
+        modulated_with_noise = clip(awgn(modulated, snr), [0, 1]);
 
         % demodulate
         demodulated = fmdemod(modulated_with_noise, 100 * 1000, sample_rate);
